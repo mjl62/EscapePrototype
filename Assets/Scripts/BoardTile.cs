ecbbf0313ce1d81557b199511192a11da646b391
mjl62 <mjl62@njit.edu>
2024-05-03 14:50:28 -0400

    Starting over (See desc)
    Removing a lot of the work done to start over, as I have different ideas for how I want to approach this.

--- Assets/Scripts/BoardTile.cs
+++ Assets/Scripts/BoardTile.cs
@@ -6,34 +6,10 @@ using UnityEngine;
 
 public class BoardTile : MonoBehaviour
 {
-    public GameObject tileNorth;
-    public GameObject tileEast;
-    public GameObject tileSouth;
-    public GameObject tileWest;
-
-    int connectedTiles = 0;
-
-    bool junction = false;
 
     private void Start()
     {
-        if (tileNorth != null) { connectedTiles += 1; }
-        if (tileEast != null) { connectedTiles += 1; }
-        if (tileWest != null) { connectedTiles += 1; }
-        if (tileSouth != null) { connectedTiles += 1; }
-
-        if (connectedTiles > 2)
-        {
-            junction = true;
-        }
+        return;
     }
-
-    public GameObject nextTile()
-    {
-        return tileNorth;
-    }
-
-    public bool isJunction() {
-        return junction;
-    }
+    
 }
